90a64e5060015701ac397403f5f81fc110666e8c
Brad Urani <bradurani@gmail.com>
2015-10-18 17:32:30 -0700

    Implement equality checking

--- lib/hamster/matrix.rb
+++ lib/hamster/matrix.rb
@@ -12,8 +12,33 @@ module Hamster
         @row_vectors = Hamster.vector(*array.map { |a| Hamster.vector(*a) })
       end
 
-      def row(n)
-        row_vectors[n]
+      def ==(other)
+        return false unless other.is_a? Hamster::Matrix::Matrix
+        self.row_vectors == other.row_vectors
+      end
+
+      def [](i,j)
+        row(i)[j]
+      end
+
+      def []=(*args)
+        raise "Requires 3 args. Call must take the form m[0,0] = 0" unless args.length == 3
+        i = args[0]
+        j = args[1]
+        value = args[2]
+        set(i,j,value)
+      end
+
+      def set(i,j,value)
+        oldRow = row(i)
+        newRow = oldRow.set(j, value)
+        puts newRow.class
+        row_vectors.set(i, newRow)
+
+      end
+
+      def row(i)
+        row_vectors[i]
       end
 
       def row_vectors
--- spec/hamster/matrix_spec.rb
+++ spec/hamster/matrix_spec.rb
@@ -8,12 +8,119 @@ end
 
 describe Hamster::Matrix::Matrix do
   
-  context '#new' do
-    it 'creates a matrix' do
-      matrix = Hamster::Matrix::Matrix.new([[1,1],[1,1]])
-      expected = Hamster.vector(Hamster.vector(1,1), Hamster.vector(1,1))
-      expect(matrix.row_vectors).to eql(expected)
+  context '#==' do
+    context 'true cases' do
+      it 'returns true if it compares to matrices with the same values' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1,1],[1,1]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,1],[1,1]])
+        expect(matrix1 == matrix2).to be true
+      end
+
+      it 'returns true if it compares to matrices with varying values' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+        expect(matrix1 == matrix2).to be true
+      end
+
+      it 'returns true if it compares two zero matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([])
+        matrix2 = Hamster::Matrix::Matrix.new([])
+        expect(matrix1 == matrix2).to be true
+      end
+
+      it 'returns true if it compares equal non-sqaure matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1,2,3],[3,4,5]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,2,3],[3,4,5]])
+        expect(matrix1 == matrix2).to be true
+      end
+
+      it 'returns true if it compares row matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1,2,3]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,2,3]])
+        expect(matrix1 == matrix2).to be true
+      end
+
+      it 'returns true if it compares column matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1],[2],[3]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1],[2],[3]])
+        expect(matrix1 == matrix2).to be true
+      end
+
+      it 'returns true if it compares matrices of any type' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1],['s'],[:foo]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1],['s'],[:foo]])
+        expect(matrix1 == matrix2).to be true
+      end
     end
+    context 'false cases' do
+       it 'returns false if it compares to matrices with the same values' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1,1],[1,1]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,1],[1,2]])
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares to matrices with varying values' do
+        matrix1 = Hamster::Matrix::Matrix.new([[0,2],[3,4]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares two zero matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([])
+        matrix2 = Hamster::Matrix::Matrix.new([[0]])
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares equal non-sqaure matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1,2,4],[3,4,5]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,2,3],[3,4,5]])
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares row matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1,2,4]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1,2,3]])
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares column matrices' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1],[8],[3]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1],[2],[3]])
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares matrices of different types' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1],[:foo],[3]])
+        matrix2 = Hamster::Matrix::Matrix.new([[1],[2],[3]])
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares matrix to nil' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1],[:foo],[3]])
+        matrix2 = nil
+        expect(matrix1 == matrix2).to be false
+      end
+
+      it 'returns false if it compares matrix to nil' do
+        matrix1 = Hamster::Matrix::Matrix.new([[1],[:foo],[3]])
+        matrix2 = "bar"
+        expect(matrix1 == matrix2).to be false
+      end
+
+    end
+  end
+
+  context '#new' do
+    it 'creates a matrix given an Array' do
+      matrix = Hamster::Matrix::Matrix.new([[1,1],[1,1]])
+      expected = Hamster::Matrix::Matrix.new([[1,1],[1,1]])
+      expect(matrix).to eql(expected)
+    end
+
+    it 'create a matrix given Hamster::Vectors' do
+       
+    end
+
   end
 
   context '#row_vectors' do
@@ -36,6 +143,49 @@ describe Hamster::Matrix::Matrix do
       expected = Hamster.vector(3,4)
       expect(matrix.row(1)).to eql(expected)
     end
+  end
+
+  context '#[]' do
+    it 'returns item at 0,0' do
+      matrix = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+      expect(matrix[0,0]).to eql(1)
+    end
+
+    it 'returns item at 0,1' do
+      matrix = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+      expect(matrix[0,1]).to eql(2)
+    end
+
+    it 'returns item at 1,0' do
+      matrix = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+      expect(matrix[1,0]).to eql(3)
+    end
+
+    it 'returns item at 1,1' do
+      matrix = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+      expect(matrix[1,1]).to eql(4)
+    end
+
+  end
+
+  context '#[]=' do
+    
+    it 'raises error if not passed 3 args' do
+      matrix = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+      expect { matrix[0] = 0 }.to raise_error("Requires 3 args. Call must take the form m[0,0] = 0")
+    end    
+
+  end
+
+  context '#set' do
+    # it 'sets item at 0,0 persistently' do
+    #   matrix = Hamster::Matrix::Matrix.new([[1,2],[3,4]])
+    #   newMatrix = matrix.set(0,0,:foo)
+    #   expect(matrix.row_vectors).to eql(Hamster.vector(Hamster.vector(1,2), 
+    #                                                    Hamster.vector(3,4)))
+    #   expect(newMatrix.row_vectors).to eql(Hamster.vector(Hamster.vector(:foo ,2), 
+    #                                                       Hamster.vector(3,4)))
+    # end
   end
 
 end
