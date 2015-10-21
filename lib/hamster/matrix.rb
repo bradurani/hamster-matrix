475567869cf8b5071ef886d8678b2b9932250d9b
Brad Urani <bradurani@gmail.com>
2015-10-20 22:32:12 -0700

    Implement class methods

--- lib/hamster/matrix.rb
+++ lib/hamster/matrix.rb
@@ -14,14 +14,58 @@ module Hamster
       Hamster::Matrix.new(array)
     end
 
+    def self.identity(n)
+      new(::Matrix.identity(n).row_vectors)
+    end
+
+    def self.build(row_count, column_count = row_count, &block)
+      matrix = ::Matrix.build(row_count, column_count, &block)
+      new(matrix.row_vectors)
+    end
+
+    def self.column_vector(array)
+      matrix = ::Matrix.column_vector(array)
+      new(matrix.row_vectors)
+    end
+
+    def self.columns(array)
+      matrix = ::Matrix.columns(array)
+      new(matrix.row_vectors)
+    end
+
+    def self.diagonal(*array)
+      matrix = ::Matrix.diagonal(*array)
+      new(matrix.row_vectors)
+    end
+
+    def self.row_vector(row)
+      matrix = ::Matrix.row_vector(row)
+      new(matrix.row_vectors)
+    end
+
+    def self.rows(row)
+      matrix = ::Matrix.rows(row)
+      new(matrix.row_vectors)
+    end
+
+    def self.scalar(n, value)
+      matrix = ::Matrix.scalar(n, value)
+      new(matrix.row_vectors)
+    end
+
+    class << self
+      alias_method :I, :identity
+      alias_method :unit, :identity
+    end
+
     def initialize(array = Hamster.vector)
       enumerable_check!(array)
       width = nil
       @row_vectors = Hamster.vector(*array.map do |a| 
         enumerable_check!(a)
-        width ||= a.length
-        unless(width == a.length)
-          raise ExceptionForMatrix::ErrDimensionMismatch.new("row size differs (#{a.length} should be #{width})")
+        width ||= a.size
+        unless(width == a.size)
+          raise ExceptionForMatrix::ErrDimensionMismatch.new("row size differs (#{a.size} should be #{width})")
         end
         Hamster.vector(*a) 
       end)
@@ -41,11 +85,11 @@ module Hamster
     alias_method :[], :get
 
     def set(i,j,value)
-      if(i >= row_vectors.length)
+      if(i >= row_vectors.size)
         raise ExceptionForMatrix::ErrDimensionMismatch.new("I index #{i} outside of array bounds")
       end
       oldRow = row(i)
-      if(j >= oldRow.length)
+      if(j >= oldRow.size)
         raise ExceptionForMatrix::ErrDimensionMismatch.new("J index #{j} outside of array bounds")
       end
       newRow = oldRow.set(j, value)
@@ -54,6 +98,15 @@ module Hamster
 
     def row(i)
       row_vectors[i]
+    end
+
+    def column_count
+      return 0 if row_vectors.size == 0
+      return row_vectors.first.size
+    end
+
+    def row_count
+      return row_vectors.size
     end
 
     attr_reader :row_vectors
--- spec/hamster/matrix_spec.rb
+++ spec/hamster/matrix_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Hamster do
-  context '#matrix' do
+  describe '#matrix' do
     it 'calls constructor' do
       expect(Hamster.matrix([[1,2,3],[1,2,3]])).to eql Hamster::Matrix.new([[1,2,3],[1,2,3]])
     end
@@ -16,11 +16,162 @@ describe Hamster::Matrix do
   it 'has a version number' do
     expect(Hamster::Matrix::VERSION).not_to be nil
   end
+
+  describe '#new' do
+    context 'valid args' do
+      it 'creates a matrix given an Array' do
+        matrix = Hamster::Matrix.new([[1,2],[3,4]])
+        expect(matrix.row_vectors.length).to eql 2
+        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
+        expect(matrix.row_vectors.first).to eql(Hamster.vector(1,2))
+        expect(matrix.row_vectors[1]).to eql(Hamster.vector(3,4))
+      end
+
+      it 'create a matrix given Hamster::Vectors' do
+        matrix = Hamster::Matrix.new(Hamster.vector(
+          Hamster.vector(1,2,3),
+          Hamster.vector(1,2,3)
+        ))
+        expected = Hamster::Matrix.new([[1,2,3],[1,2,3]])
+        expect(matrix).to eql(expected)
+        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
+        expect(matrix.row_vectors.all?{ |r| r.is_a?(Hamster::Vector)}).to be true
+      end
+
+      it 'create a matrix given Hamster::Lists' do
+        matrix = Hamster::Matrix.new(Hamster.list(
+          Hamster.list(1,2,3),
+          Hamster.list(1,2,3)
+        ))
+        expected = Hamster::Matrix.new([[1,2,3],[1,2,3]])
+        expect(matrix).to eql(expected)
+        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
+        expect(matrix.row_vectors.all?{ |r| r.is_a?(Hamster::Vector)}).to be true
+      end
+
+      it 'create a matrix given row matrix' do
+        matrix = Hamster::Matrix.new([[1,2,3]])
+        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
+        expect(matrix.row_vectors.first).to eql(Hamster.vector(1,2,3))
+      end
+
+      it 'create a matrix given nothing' do
+        matrix = Hamster::Matrix.new
+        expect(matrix.row_vectors).to eql(Hamster.vector)
+      end
+
+      it 'create a matrix given empty array' do
+        matrix = Hamster::Matrix.new([])
+        expect(matrix.row_vectors).to eql(Hamster.vector)
+      end
+    end
+
+    context 'invalid args' do
+      it 'raises a ExceptionForMatrix::ErrDimensionMismatch with mismatched dims' do
+        expect { Hamster::Matrix.new([[1,2],[1,2,3]]) }.to raise_error(ExceptionForMatrix::ErrDimensionMismatch, "row size differs (3 should be 2)")
+      end
+
+      it 'raises a ExceptionForMatrix::ErrDimensionMismatch with mismatched dims and types' do
+        expect { Hamster::Matrix.new([[1,2, :foo],['s','f']]) }.to raise_error(ExceptionForMatrix::ErrDimensionMismatch, "row size differs (2 should be 3)")
+      end
+
+      it 'raises a type error for non-enumerables' do
+        expect { Hamster::Matrix.new([[1,2],1]) }.to raise_error(TypeError)
+      end
+
+      it 'raises a type error given flat array' do
+        expect { Hamster::Matrix.new([1,2,3]) }.to raise_error(TypeError)
+      end
+
+      it 'raises a type error given flat array' do
+        expect { Hamster::Matrix.new([1,2,3]) }.to raise_error(TypeError)
+      end
+    end
+  end
+
+  describe '#identity' do
+    it 'creates an identity matrix' do
+      expect(Hamster::Matrix.identity(3)).to eql(Hamster::Matrix.new([[1,0,0],[0,1,0],[0,0,1]]))
+    end
+
+    it 'creates an empty matrix' do
+      expect(Hamster::Matrix.identity(0)).to eql(Hamster::Matrix.new)
+    end
+  end
+
+  describe '#I' do
+    it 'creates an identity matrix' do
+      expect(Hamster::Matrix.identity(3)).to eql(Hamster::Matrix.new([[1,0,0],[0,1,0],[0,0,1]]))
+    end
+  end
+
+  describe '#build' do
+    it 'builds given a block' do
+      matrix = Hamster::Matrix.build(2,2) do |i,j|
+        i * j
+      end
+      expect(matrix).to eql(Hamster::Matrix.new([[0,0],[0,1]]))
+    end
+  end
+
+  describe '#column_vector' do
+    it 'builds column matrix' do
+      matrix = Hamster::Matrix.column_vector([1,2,3])
+      expect(matrix).to eql(Hamster::Matrix.new([[1],[2],[3]]))
+    end
+
+    it 'builds column matrix from a Hamster::List' do
+      matrix = Hamster::Matrix.column_vector(Hamster::List[1,2,3])
+      expect(matrix).to eql(Hamster::Matrix.new([[1],[2],[3]]))
+    end
+  end
+
+  describe '#columns' do
+    it 'builds matrix from columns' do
+      matrix = Hamster::Matrix.columns([[1,2,3],[4,5,6]])
+      expect(matrix).to eql(Hamster::Matrix.new([[1,4],[2,5],[3,6]]))
+    end
+  end
+
+  describe '#diagonal' do
+    it 'builds matrix from diagonal' do
+      matrix = Hamster::Matrix.diagonal(1,2,3)
+      expect(matrix).to eql(Hamster::Matrix.new([[1,0,0],[0,2,0],[0,0,3]]))
+    end
+  end
+
+  describe '#row_vector' do
+    it 'builds a row matrix' do
+      matrix = Hamster::Matrix.row_vector([1,2,3])
+      expect(matrix).to eql(Hamster::Matrix.new([[1,2,3]]))
+    end
+  end
+
+  describe '#rows' do
+    it 'builds matrix' do
+      matrix = Hamster::Matrix.rows([[1,2,3],[4,5,6]])
+      expect(matrix).to eql(Hamster::Matrix.new([[1,2,3],[4,5,6]]))
+    end
+  end
+
+  describe '#scalar' do
+    it 'builds scalar matrix' do
+      matrix = Hamster::Matrix.scalar(2,5)
+      expect(matrix).to eql(Hamster::Matrix.new([[5,0],[0,5]]))
+    end
+  end
+
+  describe '#unit' do
+    it 'creates an identity matrix' do
+      expect(Hamster::Matrix.identity(3)).to eql(Hamster::Matrix.new([[1,0,0],[0,1,0],[0,0,1]]))
+    end
+  end
+
 end
 
 describe Hamster::Matrix do
   
-  context '#[]' do
+  describe '#[]' do
     it 'calls constructor' do
       expect(Hamster::Matrix[[1,2,3],[1,2,3]]).to eql Hamster::Matrix.new([[1,2,3],[1,2,3]])
     end
@@ -30,7 +181,7 @@ describe Hamster::Matrix do
     end
   end
 
-  context '#==' do
+  describe '#==' do
     context 'true cases' do
       it 'returns true if it compares to matrices with the same values' do
         matrix1 = Hamster::Matrix.new([[1,1],[1,1]])
@@ -74,6 +225,7 @@ describe Hamster::Matrix do
         expect(matrix1 == matrix2).to be true
       end
     end
+
     context 'false cases' do
        it 'returns false if it compares to matrices with the same values' do
         matrix1 = Hamster::Matrix.new([[1,1],[1,1]])
@@ -132,82 +284,7 @@ describe Hamster::Matrix do
     end
   end
 
-  context '#new' do
-    context 'valid args' do
-      it 'creates a matrix given an Array' do
-        matrix = Hamster::Matrix.new([[1,2],[3,4]])
-        expect(matrix.row_vectors.length).to eql 2
-        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
-        expect(matrix.row_vectors.first).to eql(Hamster.vector(1,2))
-        expect(matrix.row_vectors[1]).to eql(Hamster.vector(3,4))
-      end
-
-      it 'create a matrix given Hamster::Vectors' do
-        matrix = Hamster::Matrix.new(Hamster.vector(
-          Hamster.vector(1,2,3),
-          Hamster.vector(1,2,3)
-        ))
-        expected = Hamster::Matrix.new([[1,2,3],[1,2,3]])
-        expect(matrix).to eql(expected)
-        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
-        expect(matrix.row_vectors.all?{ |r| r.is_a?(Hamster::Vector)}).to be true
-      end
-
-      it 'create a matrix given Hamster::Lists' do
-        matrix = Hamster::Matrix.new(Hamster.list(
-          Hamster.list(1,2,3),
-          Hamster.list(1,2,3)
-        ))
-        expected = Hamster::Matrix.new([[1,2,3],[1,2,3]])
-        expect(matrix).to eql(expected)
-        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
-        expect(matrix.row_vectors.all?{ |r| r.is_a?(Hamster::Vector)}).to be true
-      end
-
-      it 'create a matrix given row matrix' do
-        matrix = Hamster::Matrix.new([[1,2,3]])
-        expect(matrix.row_vectors.is_a?(Hamster::Vector)).to be true
-        expect(matrix.row_vectors.first).to eql(Hamster.vector(1,2,3))
-      end
-
-      it 'create a matrix given nothing' do
-        matrix = Hamster::Matrix.new
-        expect(matrix.row_vectors).to eql(Hamster.vector)
-      end
-
-      it 'create a matrix given empty array' do
-        matrix = Hamster::Matrix.new([])
-        expect(matrix.row_vectors).to eql(Hamster.vector)
-      end
-
-    end
-
-    context 'invalid args' do
-      it 'raises a ExceptionForMatrix::ErrDimensionMismatch with mismatched dims' do
-        expect { Hamster::Matrix.new([[1,2],[1,2,3]]) }.to raise_error(ExceptionForMatrix::ErrDimensionMismatch, "row size differs (3 should be 2)")
-      end
-
-      it 'raises a ExceptionForMatrix::ErrDimensionMismatch with mismatched dims and types' do
-        expect { Hamster::Matrix.new([[1,2, :foo],['s','f']]) }.to raise_error(ExceptionForMatrix::ErrDimensionMismatch, "row size differs (2 should be 3)")
-      end
-
-      it 'raises a type error for non-enumerables' do
-        expect { Hamster::Matrix.new([[1,2],1]) }.to raise_error(TypeError)
-      end
-
-      it 'raises a type error given flat array' do
-        expect { Hamster::Matrix.new([1,2,3]) }.to raise_error(TypeError)
-      end
-
-      it 'raises a type error given flat array' do
-        expect { Hamster::Matrix.new([1,2,3]) }.to raise_error(TypeError)
-      end
-
-    end
-
-  end
-
-  context '#row_vectors' do
+  describe '#row_vectors' do
     it 'creates a matrix' do
       matrix = Hamster::Matrix.new([[1,1],[1,1]])
       expected = Hamster.vector(Hamster.vector(1,1), Hamster.vector(1,1))
@@ -215,7 +292,7 @@ describe Hamster::Matrix do
     end
   end
 
-  context '#row' do
+  describe '#row' do
     it 'returns row at 0' do
       matrix = Hamster::Matrix.new([[1,2],[3,4]])
       expected = Hamster.vector(1,2)
@@ -229,7 +306,7 @@ describe Hamster::Matrix do
     end
   end
 
-  context '#get' do
+  describe '#get' do
     it 'returns item at 0,0' do
       matrix = Hamster::Matrix.new([[1,2],[3,4]])
       expect(matrix.get(0,0)).to eql(1)
@@ -262,14 +339,14 @@ describe Hamster::Matrix do
 
   end
   
-  context '#[]' do
+  describe '#[]' do
     it 'calls #get' do
       matrix = Hamster::Matrix.new([[1,2],[3,4]])
       expect(matrix[0,0]).to eql(1)
     end
   end
 
-  context '#set' do
+  describe '#set' do
     it 'sets item at 0,0 persistently' do
       matrix = Hamster::Matrix.new([[1,2],[3,4]])
       newMatrix = matrix.set(0,0,:foo)
@@ -294,6 +371,45 @@ describe Hamster::Matrix do
       expect { matrix.set(0,20,"foo") }.to raise_error(ExceptionForMatrix::ErrDimensionMismatch, 'J index 20 outside of array bounds')
     end
 
+  end
+
+  describe '#column_count' do
+    it 'gets column count' do
+      matrix = Hamster::Matrix.new([[1,2,3],['a','b','c']])
+      expect(matrix.column_count).to eql(3)
+    end
+
+    it 'gets column count for row matrix' do
+      matrix = Hamster::Matrix.new([[1,2,3]])
+      expect(matrix.column_count).to eql(3)
+    end
+
+    it 'gets column count for empty matrix' do
+      matrix = Hamster::Matrix.new
+      expect(matrix.column_count).to eql(0)
+    end
+  end
+
+  describe '#row_count' do
+    it 'gets row count' do
+      matrix = Hamster::Matrix.new([[1,2,3],['a','b','c']])
+      expect(matrix.row_count).to eql(2)
+    end
+
+    it 'gets row count for row matrix' do
+      matrix = Hamster::Matrix.new([[1,2,3]])
+      expect(matrix.row_count).to eql(1)
+    end
+
+    it 'gets column count for column matrix' do
+      matrix = Hamster::Matrix.new([[1],[2],[3]])
+      expect(matrix.row_count).to eql(3)
+    end
+
+    it 'gets column count for empty matrix' do
+      matrix = Hamster::Matrix.new
+      expect(matrix.row_count).to eql(0)
+    end
   end
 
 end
